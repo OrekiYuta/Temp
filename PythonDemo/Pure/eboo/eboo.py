02323c43814d270d6800e613503ebe166700b2d6
Elias <orekiyuta@gmail.com>
2021-01-25 23:30:57 +0800

    2021-01-25/23:30:57.45 by auto commit

--- PythonDemo/Pure/eboo/eboo.py
+++ PythonDemo/Pure/eboo/eboo.py
@@ -1,26 +1,37 @@
+import os
 import time
 import mariadb
 import requests
-
+import yaml
 from randomUA import getUserAgent
 from datetime import datetime
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException, NoAlertPresentException
+from selenium.webdriver import Chrome, ChromeOptions, ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 phoneNum = 18800126264
+# phoneNum = 18800126263
 
 
 def ChromeOpen():
     # headers = f"user-agent='{getUserAgent()}'"
-    headers = 'user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0" '
+    headers = 'user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36" '
     print(headers)
     options = ChromeOptions()
     options.add_argument(headers)
     # options.add_argument("--headless")  # 隐藏浏览器
     options.add_argument("--no-sandbox")  # linux 需要禁用这个
+    options.add_argument("–incognito")
     options.add_argument("--disable-gpu")
     options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片资源
+    prefs = {
+        'profile.default_content_setting_values': {
+            'notifications': 2
+        }
+    }
+    options.add_experimental_option('prefs', prefs)  # 禁止弹窗
     # chrome = Chrome(executable_path="./chromedriver.exe", options=options) # 路径加载驱动
     chrome = Chrome(options=options)
 
@@ -44,36 +55,141 @@ config = [{'name': 'qncyw', 'nameCn': '青年创业网', 'url': 'https://www.qnc
            'phoneInputParser': '//*[@name="identityId"]',
            'sendButtonParser': '//*[@id="regPersonalForm"]/ul/li[5]/div[1]/em/a'},
           ]
+'''
+# 获取当前文件路径 F:/Code/Temp/PythonDemo/Pure/eboo
+print(os.path.dirname(__file__))
+# 获取当前文件 realpath  ('F:\\Code\\Temp\\PythonDemo\\Pure\\eboo', 'eboo.py')
+print(os.path.split(os.path.realpath(__file__)))
+# F:\Code\Temp\PythonDemo\Pure\eboo
+filepath = os.path.split(os.path.realpath(__file__))[0]
+print(filepath)
+# 获取与 filepath 同级目录下的文件
+path_join = os.path.join(filepath, 'properties.yaml')
+print(path_join)
+
+filename = os.path.join(os.path.dirname(__file__), 'properties.yaml'.replace("\\", "/"))
+t = open(filename, encoding='utf-8')
+print(yaml.load(t, Loader=yaml.FullLoader))
+'''
+
+file_path = os.path.join(os.path.dirname(__file__), 'properties.yaml'.replace("\\", "/"))
+yaml_load = yaml.load(open(file_path, encoding='utf-8'), Loader=yaml.FullLoader)
+# print(yaml_load)
+# print(type(yaml_load))
+'''
+for prop in yaml_load:
+    # print(prop['title'])
+    print(type(prop))
+    # for site in prop:
+    #     print(site)
+'''
 
 
-def eboo(chrome):
-    for item in config:
-        itemName = item['name']
-        itemUrl = item['url']
-        itemParser = ''
-        setDateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+# for site in yaml_load['Sites']:
+#     print(site['url'])
 
-        try:
+
+def getProperties():
+    file_path = os.path.join(os.path.dirname(__file__), 'properties.yaml'.replace("\\", "/"))
+    yaml_load = yaml.load(open(file_path, encoding='utf-8'), Loader=yaml.FullLoader)
+    return yaml_load
+
+
+# def __call__(self, driver):
+#     try:
+#         alert = driver.switch_to.alert
+#         alert.text
+#         return alert
+#     except NoAlertPresentException:
+#         return False
+
+
+def ebooyaml(chrome):
+    for item in yaml_load['Sites']:
+        # 判断几个参数同时不为空
+        if all([item['url'], item['name'], item['phoneInputParser'], item['sendButtonParser']]):
+            itemUrl = item['url']
+            itemName = item['name']
+            itemParser = 'xpath'
+            setDateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+            # chrome.switch_to.alert().dismiss()
             chrome.get(itemUrl)
+            chrome.refresh()  # 刷新一下，有的站点在启动浏览器的时候检测了是否开启自动化工具
+
+            time.sleep(1)
 
             if item['parser'] == 'selector':
                 itemParser = 'find_element_by_css_selector'
             elif item['parser'] == 'xpath':
                 itemParser = 'find_element_by_xpath'
+            else:
+                print(f"parser: '{item['parser']}' is Invalid Parameter")
+                continue
 
+            # print("-----------------------------------------------------------")
+            print(f"{setDateTime} -- {itemName} start ")
             # chrome 对象通过 __getattribute__ 方法去执行名为 itemParser 变量值的函数
-            chrome.__getattribute__(itemParser)(item['phoneInputParser']).send_keys(phoneNum)
-            chrome.__getattribute__(itemParser)(item['sendButtonParser']).click()
 
-        except NoSuchElementException as error:
-            print(f"{itemName} ---> {error}")
-            chrome.get_screenshot_as_file('./error/' + itemName + '_' + setDateTime + '.png')
-        finally:
-            print(f"{itemName} already completed at {setDateTime}")
+            try:
+                chrome.__getattribute__(itemParser)(item['phoneInputParser']).send_keys(phoneNum)
+                # 有的页面在输入后需要移出光标做校验完毕才显示验证码按钮
+                # ActionChains(chrome).move_by_offset(10, 200).perform()  # 左键点击
+                ActionChains(chrome).move_by_offset(0, 0).context_click().perform()  # 右键
+                time.sleep(1)
+                chrome.__getattribute__(itemParser)(item['sendButtonParser']).click()
+
+            except Exception as error:
+                print(f"{itemName} ---> {error}")
+                print(f"{setDateTime} -- {itemName} expectation failed")
+                # chrome.get_screenshot_as_file('./error/' + itemName + '_' + setDateTime + '.png')
+                continue
+
+            # 判断是否存在弹窗提示
+            chromeAlert = expected_conditions.alert_is_present()(chrome)
+            if chromeAlert:
+                print(chromeAlert.text)
+                # print(chrome.switch_to.alert.text)
+                chrome.switch_to.alert.dismiss()
+
+            print(f"{setDateTime} -- {itemName} completed")
+            print("-----------------------------------------------------------")
+
+            # finally:
+            #     print(f"{itemName} already completed at {setDateTime}")
+            #     print("-----------------------------------------------------------")
+        else:
+            print("Params[url,name,phoneInputParser,sendButtonParser] Can't be Null")
 
 
 chrome_open = ChromeOpen()
-eboo(chrome_open)
+ebooyaml(chrome_open)
+# ChromeClose(chrome_open)
+
+# def eboo(chrome):
+#     for item in config:
+#         itemName = item['name']
+#         itemUrl = item['url']
+#         itemParser = ''
+#         setDateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+#
+#         try:
+#             chrome.get(itemUrl)
+#
+#             if item['parser'] == 'selector':
+#                 itemParser = 'find_element_by_css_selector'
+#             elif item['parser'] == 'xpath':
+#                 itemParser = 'find_element_by_xpath'
+#
+#             # chrome 对象通过 __getattribute__ 方法去执行名为 itemParser 变量值的函数
+#             chrome.__getattribute__(itemParser)(item['phoneInputParser']).send_keys(phoneNum)
+#             chrome.__getattribute__(itemParser)(item['sendButtonParser']).click()
+#
+#         except NoSuchElementException as error:
+#             print(f"{itemName} ---> {error}")
+#             chrome.get_screenshot_as_file('./error/' + itemName + '_' + setDateTime + '.png')
+#         finally:
+#             print(f"{itemName} already completed at {setDateTime}")
 
 
 '''
