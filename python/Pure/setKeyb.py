d115c07a92e0b9945260aed630442ab923ac488c
Elias <orekiyuta@gmail.com>
2021-02-01 11:22:12 +0800

    2021-02-01/11:22:11.59 by auto commit

--- python/Pure/setKeyb.py
+++ python/Pure/setKeyb.py
@@ -1,14 +1,21 @@
+from colorama.win32 import windll
 from pynput.keyboard import Key, Listener
+import logging
+import win32clipboard
+import win32gui
 
+# def on_press(key):
+#     print(f"{key} click")
+#
+# def on_release(key):
+#     if key == Key.esc:
+#         return False
+#
+#
+# with Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
 
-def on_press(key):
-    print(f"{key} click")
-
-
-def on_release(key):
-    if key == Key.esc:
-        return False
-
-
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+# user32 = windll.user32
+# hwnd = user32.GetForegroundWindow()
+# app = win32gui.GetWindowText(hwnd)
+# print(app)
